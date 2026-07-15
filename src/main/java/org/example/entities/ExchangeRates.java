f54203b2f6549975e22d749c67e9c7526a259891
alesia <alesia>
2026-07-15 10:58:56 +0300

    bd and entities (я забыла разбить на ветки, дальше исправлю)

---
+++ src/main/java/org/example/entities/ExchangeRates.java
@@ -0,0 +1,29 @@
+package org.example.entities;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "exchange_rates")
+@Getter
+@Setter
+public class ExchangeRates {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  @ManyToOne
+  @JoinColumn(name = "currency_id")
+  private Currencies currencyId;
+
+  @Column(name = "rate", nullable = false)
+  private String rate;
+
+  @Column(name = "scale", nullable = false)
+  private long scale;
+
+  @Column(name = "rateDate", nullable = false)
+  private java.sql.Date rateDate;
+
+}

7f356491416d615dfa605f6ac56717201856fbde
alesia <alesia>
2026-07-15 11:35:34 +0300

    create ExchangeRateRepository

--- src/main/java/org/example/repositiries/ExchangeRateRepository.java
+++ src/main/java/org/example/repositiries/ExchangeRateRepository.java
@@ -1,9 +1,10 @@
 package org.example.repositiries;
 
+import org.example.entities.ExchangeRates;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ExchangeRateRepository extends JpaRepository<ExchangeRateRepository, Long> {
+public interface ExchangeRateRepository extends JpaRepository<ExchangeRates, Long> {
 
 }

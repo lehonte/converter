4b0c5442df5abf2af3787edb17ba463560e16ffe
alesia <alesia>
2026-07-15 13:26:59 +0300

    step 3 made

---
+++ src/main/java/org/example/connectors/NbrbConnector.java
@@ -0,0 +1,31 @@
+package org.example.connectors;
+
+import lombok.RequiredArgsConstructor;
+import org.example.dto.NbrbRateDto;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class NbrbConnector {
+
+    private final RestClient restClient;
+
+    public List<NbrbRateDto> getNbrbRates(LocalDate date) {
+        return restClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/exrates/rates")
+                        .queryParam("periodicity", 0)
+                        .queryParamIfPresent("ondate", Optional.ofNullable(date))
+                        .build())
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<NbrbRateDto>>() {});
+
+
+    }
+}
